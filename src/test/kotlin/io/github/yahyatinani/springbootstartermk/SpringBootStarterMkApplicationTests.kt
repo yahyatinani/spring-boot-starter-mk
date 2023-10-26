306044f11e0f1bb6435804940afb73182f7b39a9
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-10-26 19:54:47 +0100

    Add spring boot starter project v3.2.0-RC1 with Java 17

---
+++ src/test/kotlin/io/github/yahyatinani/springbootstartermk/SpringBootStarterMkApplicationTests.kt
@@ -0,0 +1,12 @@
+package io.github.yahyatinani.springbootstartermk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootStarterMkApplicationTests {
+  @Test
+  fun nothing() {
+    assert(1 + 1 == 2)
+  }
+}
