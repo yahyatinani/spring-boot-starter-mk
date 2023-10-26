c331dc9e5ac51c939ccc3af27403582c985c01ae
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-10-26 20:19:17 +0100

    Add spring boot starter project v3.2.0-RC1 with Java 17 and Gradle

---
+++ src/main/kotlin/io/github/yahyatinani/springbootstarter/SpringBootStarterMkApplication.kt
@@ -0,0 +1,11 @@
+package io.github.yahyatinani.springbootstarter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootStarterMkApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootStarterMkApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/yahyatinani/springbootstarter/SpringBootStarterMkApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.yahyatinani.springbootstarter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootStarterMkApplicationTests {
+
+  @Test
+  fun nothing() {
+    assert(1 + 1 == 2)
+  }
+}
