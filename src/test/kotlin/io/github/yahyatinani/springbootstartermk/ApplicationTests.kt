2881d527737db0de374e48789756c83f703325f4
Yahya Tinani <47183730+yahyatinani@users.noreply.github.com>
2023-11-08 20:00:02 +0100

    Rename the default test

--- src/test/kotlin/io/github/yahyatinani/springbootstartermk/ApplicationTests.kt
+++ src/test/kotlin/io/github/yahyatinani/springbootstartermk/ApplicationTests.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class ApplicationTests {
   @Test
-  fun nothing() {
+  fun loadSpringApplicationContext() {
     assert(1 + 1 == 2)
   }
 }
